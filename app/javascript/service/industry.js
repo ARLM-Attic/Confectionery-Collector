be2f1f683abacc10ee6fe1193657140c23418854
Sdonai <silentstrum@gmail.com>
2015-05-05 22:19:14 -0700

    fix style in industry service

--- app/javascript/service/industry.js
+++ app/javascript/service/industry.js
@@ -1,99 +1,99 @@
 //write code to manage prisoners
-(function(){
-	var industry = function($http, $interval){
-		var comodity = function(name, sell, buy){
+(function() {
+	var industry = function($http, $interval) {
+		var comodity = function(name, sell, buy) {
 			var count = 0;
 			var efficiency = 0.1;
 			self = {};
 			self.name = name;
 			self.getSell = sell;
 			self.getBuy = buy;
-			self.achievements = function(){console.log("Empty Achievements")};
+			self.achievements = function() {console.log('Empty Achievements')};
 
-			self.getCount = function(){
-				return count; 
-			}
+			self.getCount = function() {
+				return count;
+			};
 
-			self.makeItem = function(amount){
-				chai.assert.isNumber(amount, 'string')
+			self.makeItem = function(amount) {
+				chai.assert.isNumber(amount, 'string');
 				count += amount * efficiency;
-			}
+			};
 
-			self.useItem = function(cost){
-				count -= cost
-			}
+			self.useItem = function(cost) {
+				count -= cost;
+			};
 
-			self.buy = function(amount){
-				expect(amount).to.be.ok.and.a('number')
-				if(amount && sugar.getCount() >= buy * amount){
+			self.buy = function(amount) {
+				expect(amount).to.be.ok.and.a('number');
+				if (amount && sugar.getCount() >= buy * amount) {
 					count += amount || 1;
-					sugar.useItem(buy * amount)
-					self.saveState()
+					sugar.useItem(buy * amount);
+					self.saveState();
 				}
-			}
+			};
 
-			self.sell = function(){
-				if(count >= 1){
+			self.sell = function() {
+				if (count >= 1) {
 					sugar.buy(count * sell);
 					count = 0;
 					self.saveState();
 				}
-			}
+			};
 
-			self.setAcheivements = function(achievement){
-				self.achievements = achievement
-			}
+			self.setAcheivements = function(achievement) {
+				self.achievements = achievement;
+			};
 
-			self.saveState = function(){
-				window.localStorage.setItem('industry-' + name, count)
+			self.saveState = function() {
+				window.localStorage.setItem('industry-' + name, count);
 
-			}
+			};
 
-			self.loadState = function(){
+			self.loadState = function() {
 				var state = Number(window.localStorage.getItem('industry-' + name));
-				if(!state){
+				if (!state) {
 					count = Number(sessionStorage.getItem('industry-' + name)) || 0;
 					self.saveState();
-				} else if(state){
-					count = state
+				} else if (state) {
+					count = state;
 				}
-			}
+			};
 			return self;
-		}
+		};
 
-		var sugar = new comodity("Sugar", 0, 0);
+		var sugar = new comodity('Sugar', 0, 0);
 
 		var comodities = {
-			sugar : sugar,
-			taffy : new comodity("Taffy", 10, 15),
-			jellyBean: new comodity("JellyBean", 30, 50),
-			doughNut: new comodity("Doughnut", 100, 600),
-			jawBreaker: new comodity("Jawbreaker", 250, 1000),
-			peppermint: new comodity("Peppermint", 600, 3000),
-			rockCandy: new comodity("Rock-Candy", 1500, 9000)
+			sugar: sugar,
+			taffy: new comodity('Taffy', 10, 15),
+			jellyBean: new comodity('JellyBean', 30, 50),
+			doughNut: new comodity('Doughnut', 100, 600),
+			jawBreaker: new comodity('Jawbreaker', 250, 1000),
+			peppermint: new comodity('Peppermint', 600, 3000),
+			rockCandy: new comodity('Rock-Candy', 1500, 9000)
 		};
 
 		//load all the states
-		for(name in comodities){
-			comodities[name].loadState()
+		for (name in comodities) {
+			comodities[name].loadState();
 		}
 
 		//save every so often
-		var save = $interval(function saveComodities(){
-			for(name in comodities){
-				comodities[name].saveState()
+		var save = $interval(function saveComodities() {
+			for (name in comodities) {
+				comodities[name].saveState();
 			}
 		},
 		30000);
 
 		//expose comodity in prototype for unit tests
-		Object.defineProperty(comodities, "comodity", {
+		Object.defineProperty(comodities, 'comodity', {
 		  enumerable: false,
 		  value: comodity
 		});
 
 		//expose for unit tests
-		Object.defineProperty(comodities, "save", {
+		Object.defineProperty(comodities, 'save', {
 		  enumerable: false,
 		  value: save
 		});
@@ -102,6 +102,6 @@
 
 	};
 
-	var module = angular.module("marcApp")
-	module.factory("industry", industry)
-}())
+	var module = angular.module('marcApp');
+	module.factory('industry', industry);
+}());
